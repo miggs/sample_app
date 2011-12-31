a1389cd83f6d306def99db4105aaa0d140c7d90d
Mark Myers <dog.otter@gmail.com>
2011-12-30 22:11:54 -0600

    Finished layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,7 +5,7 @@ describe PagesController do
 
   before(:each) do
     #
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Ruby on Rails Tutorial Sample App" 
     #
   end
 
